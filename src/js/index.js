b0d56c508dabf9ed09e444fe18c4ccee7f0d02b2
Gabriela Jablonska <jablonska.gabriela123@gmail.com>
2020-11-12 00:20:58 +0000

    added application features

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,26 @@ registerSW();
 /* place your code below */
 
 const add = document.querySelector(".add--js");
-const delete = document.querySelector(".delete--js");
-const counter = document.querySelector('.counter--js');
+
+const deleted = document.querySelector(".delete--js");
+
+const counter = document.querySelector(".counter--js");
+
+const key = new Date().toISOString().slice(0, 10);
+
+localStorage.setItem("date", key);
+
+console.log(localStorage.getItem("date"));
+
+let i = 0;
+
+add.addEventListener("click", () => {
+  i = i + 1;
+  counter.innerHTML = i;
+});
+
+deleted.addEventListener("click", () => {
+  if(i>0){
+  i = i - 1;
+  counter.innerHTML = i;}
+});
