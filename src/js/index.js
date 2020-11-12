b4be7d6a05aea0563c4a6d401de94acb221dfd34
Gabriela Jablonska <jablonska.gabriela123@gmail.com>
2020-11-12 01:37:28 +0000

    added application features

--- src/js/index.js
+++ src/js/index.js
@@ -14,19 +14,48 @@ const counter = document.querySelector(".counter--js");
 
 const key = new Date().toISOString().slice(0, 10);
 
-localStorage.setItem("date", key);
-
-console.log(localStorage.getItem("date"));
 
 let i = 0;
 
-add.addEventListener("click", () => {
+add.addEventListener("click", (e) => {
+  e.preventDefault();
   i = i + 1;
   counter.innerHTML = i;
+  localStorage.setItem(key,i);
+
 });
 
-deleted.addEventListener("click", () => {
+deleted.addEventListener("click", (e) => {
+  e.preventDefault();
   if(i>0){
   i = i - 1;
-  counter.innerHTML = i;}
+  counter.innerHTML = i;
+  localStorage.setItem(key,i);
+  }
 });
+const value=localStorage.getItem(key);
+if(value) {
+  counter.innerHTML=value;
+  let j = counter.innerHTML;
+
+add.addEventListener("click", (e) => {
+  e.preventDefault();
+  j++;
+  counter.innerHTML = j;
+  localStorage.setItem(key,j);
+
+});
+
+deleted.addEventListener("click", (e) => {
+  e.preventDefault();
+  if(j>0){
+  j = j - 1;
+  counter.innerHTML = j;
+  localStorage.setItem(key,j);
+  }
+});
+  
+}
+else{
+  counter.innerHTML=0;
+}
